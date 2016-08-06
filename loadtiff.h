5e84c94d7a6a0b44fbb2ed4747f33b70648cdb58
Malcolm <malcolm.mclean5@btinternet.com>
2016-08-06 14:04:47 +0100

    added usage example to loadtiff.h

--- loadtiff.h
+++ loadtiff.h
@@ -3,6 +3,26 @@
 
 #include <stdio.h>
 
+/*
+  By Malcolm McLean
+
+
+  To use
+  int width, height;
+  unsigned char *rgba;
+  FILE *fp = fopen("tiffile.tiff", "rb");
+  if(!fp)
+     errorhandling();
+  rgba = floadtiff(fp, &width, &height);
+  fclose(fp);
+  
+  if(rgba == 0)
+     printf("TIFF file unreadable\n");
+  /* 
+     rgba is a red, green, blue, alpha 32 bit buffer,
+     width is image width, height is image height in pixels
+  */
+
 unsigned char *floadtiff(FILE *fp, int *width, int *height);
 
 #endif
